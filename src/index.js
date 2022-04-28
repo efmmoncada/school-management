ac9bd1bdf4d3ef60dd8574a6f649945511877945
markser <serm@oregonstate.edu>
2022-04-28 08:33:35 -0700

    using WHATWG URL standard instead of url.parse

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import url from "url";
+// The url module provides two APIs for working with URLs: a legacy API that is Node.js specific, 
+  // and a newer API that implements the same WHATWG URL Standard used by web browsers.
+// import url from "url";
 import pkg from "../package.json";
 
 const siteURL = pkg.homepage;
-const basename = siteURL ? url.parse(siteURL).pathname : "";
+const basename = siteURL ? new URL(siteURL).pathname : "";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -17,3 +19,9 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
+
+// module.exports = {
+//   resolve: {
+//     fallback: { "url": require.resolve("url/") }
+//   }
+// }
