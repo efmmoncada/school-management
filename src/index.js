db81cf6366fdd3bd71cfc992368741c9c35f8e43
efmmoncada <emmanuel.moncada27@gmail.com>
2022-04-27 23:14:33 -0700

    add router basename

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import url from "url";
+import pkg from "../package.json";
+
+const siteURL = pkg.homepage;
+const basename = siteURL ? url.parse(siteURL).pathname : "";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
