21e87205c1f4b74eccb53c1b3ba36c63e12974f4
markser <serm@oregonstate.edu>
2022-05-06 10:44:06 -0700

    linting

--- src/pages/Hosts/Hosts.js
+++ src/pages/Hosts/Hosts.js
@@ -1,14 +1,14 @@
 import AddHostsForm from "../../components/AddHostsForm/AddHostsForm";
 import "./Hosts.css";
 import React from 'react';
-import Hosts_Table from '../../components/HostsTable';
-import Hosts_Data from '../../data/Hosts'
+import HostsTable from '../../components/HostsTable';
+import HostsData from '../../data/Hosts'
 
 const Hosts = () => {
   return (
     <div>
       <h2>Here is a list of Hosts in your database.</h2>
-      <Hosts_Table items={Hosts_Data}/>
+      <HostsTable items={HostsData}/>
       <AddHostsForm />
     </div>
   );
