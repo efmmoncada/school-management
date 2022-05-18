fba319ce68de138ddd22016e2fadc8c0563604dc
mark <serm@oregonstate.edu>
2022-05-18 12:50:02 -0700

    simple get request now working

--- db/app.py
+++ db/app.py
@@ -1,5 +1,6 @@
 import MySQLdb
-import flask
+from flask import Flask, request, render_template
+from flask_cors import CORS
 import configparser
 
 
@@ -8,18 +9,32 @@ def read_mysql_config(mysql_config_file_name: str):
     config.read(mysql_config_file_name)
     return dict(config['client'])
 
-# rename the db_login_[onid] or comment out :)
-config_info = read_mysql_config("./db_login_serm.js")
-
-db_conn = MySQLdb.connect(config_info['host'],
+def connect_to_database(config_info):
+    '''
+    connects to a database and returns a database objects
+    '''
+    db_conn = MySQLdb.connect(config_info['host'],
                           config_info['user'],
                           config_info['password'],
                           config_info['database'])
+    return db_conn
 
-webapp = flask.Flask(__name__, static_url_path='/static')
 
-@webapp.route('/')
-def get_tables():
+
+
+
+# just in case we have CORS issues
+app = Flask(__name__)
+cors = CORS(app)
+
+config_info = read_mysql_config("./db_login_serm.js")
+db_conn = connect_to_database(config_info)
+
+
+
+
+@app.route('/getTables', methods=["GET"])
+def get_all_tables():
     res_html = "<html>\n<body>\n<table border=\"1\">\n"
     cursor = db_conn.cursor()
     cursor.execute('show tables;', ())
@@ -28,3 +43,24 @@ def get_tables():
     res_html += "</table>\n"
     res_html += "<img src=\"/static/logo.png\" />\n</body>\n</html>\n"
     return res_html
+
+
+# @webapp.route('/')
+# def get_tables():
+#     res_html = "<html>\n<body>\n<table border=\"1\">\n"
+#     cursor = db_conn.cursor()
+#     cursor.execute('show tables;', ())
+#     for [table_name] in cursor.fetchall():
+#         res_html += f"<tr><td>{table_name}</td></tr>\n"
+#     res_html += "</table>\n"
+#     res_html += "<img src=\"/static/logo.png\" />\n</body>\n</html>\n"
+#     return res_html
+
+
+# if __name__ == '__main__':
+#     print("Executing a sample query on the database using the credentials from db_login_[onid].py")
+#     # rename the db_login_[onid] or comment out :)
+#     config_info = read_mysql_config("./db_login_serm.js")
+#     db = connect_to_database(config_info)
+
+# webapp = flask.Flask(__name__, static_url_path='/static')
