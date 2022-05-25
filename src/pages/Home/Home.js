098e61b0a8919dd1bbd03e2ee0911c09df2d6ab8
efmmoncada <emmanuel.moncada27@gmail.com>
2022-05-24 22:26:17 -0700

    add footer to homepage with link to GH.

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,34 +1,53 @@
-import Header from "../../components/Header/Header";
+import Header from '../../components/Header/Header';
 
-import "./Home.css";
+import './Home.css';
 
 const Home = () => {
-  return (
-    <div className='home'>
-      <Header title='Home' />
+    return (
+        <div className='home'>
+            <Header title='Home' />
 
-      <p className='description'>
-        Welcome the database explorer for the school management system.
-        <br />
-        Use the links above to explore and edit the school's recoreds.
-      </p>
+            <p className='description'>
+                Welcome the database explorer for the school management system.
+                <br />
+                Use the links above to explore and edit the school's recoreds.
+            </p>
 
-      <iframe
-        title='Capoo cat'
-        src='https://giphy.com/embed/3ov9jZ0V6gOO0oa98Y'
-        width='480'
-        height='450'
-        frameBorder='0'
-        className='giphy-embed'
-        allowFullScreen
-      ></iframe>
-      <p>
-        <a href='https://giphy.com/gifs/capoo-cat-3ov9jZ0V6gOO0oa98Y'>
-          via GIPHY
-        </a>
-      </p>
-    </div>
-  );
+            <iframe
+                title='Capoo cat'
+                src='https://giphy.com/embed/3ov9jZ0V6gOO0oa98Y'
+                width='480'
+                height='450'
+                frameBorder='0'
+                className='giphy-embed'
+                allowFullScreen
+            ></iframe>
+            <p>
+                <a href='https://giphy.com/gifs/capoo-cat-3ov9jZ0V6gOO0oa98Y'>
+                    via GIPHY
+                </a>
+            </p>
+
+            <div>
+                {/* This app was bootstrapped with{' '}
+                <a
+                    href='https://create-react-app.dev'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                >
+                    Create React App.
+                </a> */}
+                Check out the source code for this page on{' '}
+                <a
+                    href='https://github.com/efmmoncada/school-management'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                >
+                    Github
+                </a>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
